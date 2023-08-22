d030f9ac6c148e75334e5532ca89f35746354eb2
sewn <sewn@disroot.org>
2023-08-22 21:22:37 +0300

    ok

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/vinegarhq/avana
 
 go 1.20
+
+require (
+	github.com/ncruces/zenity v0.10.10
+	github.com/vinegarhq/vinegar v1.4.1
+)
+
+require (
+	github.com/akavel/rsrc v0.10.2 // indirect
+	github.com/dchest/jsmin v0.0.0-20220218165748-59f39799265f // indirect
+	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
+	github.com/randall77/makefat v0.0.0-20210315173500-7ddd0e42c844 // indirect
+	golang.org/x/image v0.11.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,10 +2,81 @@ package main
 
 import (
 	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 
+	"github.com/ncruces/zenity"
+	"github.com/vinegarhq/vinegar/roblox"
+	"github.com/vinegarhq/vinegar/roblox/bootstrapper"
 	"github.com/vinegarhq/avana/internal/dirs"
 )
 
+var dialog zenity.ProgressDialog
+
+// this is horrible i hate it
+func dialogerror(err error) {
+	zenity.Error(err.Error(),
+		zenity.Title("Avana"),
+		zenity.ErrorIcon,
+	)
+
+	panic(err)
+}
+
+func SetupBinary(ver roblox.Version, dir string) {
+	if err := dirs.Mkdirs(dir, dirs.Downloads); err != nil {
+		dialogerror(err)
+	}
+
+	manifest, err := bootstrapper.FetchManifest(ver, dirs.Downloads)
+	if err != nil {
+		dialogerror(err)
+	}
+
+	if err := SetupManifest(&manifest, dir); err != nil {
+		dialogerror(err)
+	}
+}
+
 func main() {
+	var err error
+
+	dialog, err = zenity.Progress(
+		zenity.Title("Avana"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	defer dialog.Close()
+
 	log.Println(dirs.Data)
+
+	dialog.Text("Fetching Version")
+
+	ver, err := roblox.LatestVersion(roblox.Player, "LIVE")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	verDir := filepath.Join(dirs.Versions, ver.GUID)
+
+	_, err = os.Stat(filepath.Join(verDir, "AppSettings.xml"))
+	if err != nil {
+		dialog.Text("Updating/Installing Player")
+		SetupBinary(ver, verDir)
+	}
+	
+	dialog.Text("Running Player")
+
+	cmd := exec.Command(filepath.Join(verDir, roblox.Player.Executable()))
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+
+	dialog.Close()
+
+	err = cmd.Run()
+	if err != nil {
+		dialogerror(err)
+	}
 }
